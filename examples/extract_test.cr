9aa440232abaf92b04ddce06f64f8b5d20d203f7
soya_daizu <soya_daizu@proton.me>
2023-03-06 09:32:48 +0900

    feat: update sample code

--- examples/extract_test.cr
+++ examples/extract_test.cr
@@ -1,25 +1,54 @@
 require "stumpy_png"
 require "../src/goishi"
 
-def extract(input_name : String, output_name : String)
+def read_file(input_name : String)
   img = StumpyPNG.read(input_name)
-
   matrix = Goishi::Matrix(Tuple(UInt8, UInt8, UInt8)).new(img.width, img.height) do |i|
     img.pixels[i].to_rgb8
   end
 
-  matrix = Goishi::Preprocessor.binarize(matrix)
-  locator = Goishi::Locator.new(matrix)
-  location = locator.locate_qr.first
-  extractor = Goishi::Extractor.new(matrix, location)
-  matrix = extractor.extract
+  Goishi::Binarizer.binarize(matrix)
+end
 
+def write_file(output_name : String, matrix : Goishi::Matrix(UInt8))
   canvas = StumpyPNG::Canvas.new(matrix.size_x, matrix.size_y) do |x, y|
     StumpyPNG::RGBA.from_gray_n(matrix[x, y] == 1 ? 0 : 255, 8)
   end
+
   StumpyPNG.write(canvas, output_name)
 end
 
+def extract(input_name : String, output_name : String)
+  puts input_name
+  matrix = read_file(input_name)
+
+  locator = Goishi::Locator.new(matrix)
+  locator.locate_qr(20) do |location|
+    puts location
+    extractor = Goishi::Extractor.new(matrix)
+    extracted_matrix = extractor.extract(location) rescue next
+
+    decoder = Goishi::Decoder.new(extracted_matrix)
+    actual_version = decoder.read_version
+    next if actual_version < 1
+
+    if location.version != actual_version
+      location.version = actual_version
+      extracted_matrix = extractor.extract(location) rescue next
+      decoder = Goishi::Decoder.new(extracted_matrix)
+
+      # Double check the version corrected
+      next unless decoder.read_version == actual_version
+    end
+
+    decoder.decode
+
+    write_file(output_name, extracted_matrix)
+    break
+  end
+end
+
+# extract("examples/assets/test.png", "examples/assets/test_b.png")
 # extract("examples/assets/skewed1.png", "examples/assets/skewed1_b.png")
 # extract("examples/assets/skewed2.png", "examples/assets/skewed2_b.png")
 # extract("examples/assets/hflipped.png", "examples/assets/hflipped_b.png")
@@ -27,6 +56,6 @@ end
 # 0.step(to: 330, by: 30) do |i|
 #  extract("examples/assets/rotate#{i}.png", "examples/assets/rotate#{i}_b.png")
 # end
-# 4.times do |i|
-#  extract("examples/assets/rotate#{i + 1}.png", "examples/assets/rotate#{i + 1}_b.png")
-# end
+5.times do |i|
+ extract("examples/assets/real_world#{i + 1}.png", "examples/assets/real_world#{i + 1}_b.png")
+end
