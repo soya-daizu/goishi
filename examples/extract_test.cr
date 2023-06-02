0ef24bae60f33d7119f593819c11f0c3e69ad2e2
soya_daizu <soya_daizu@proton.me>
2023-06-02 20:43:32 +0900

    feat: update test code

--- examples/extract_test.cr
+++ examples/extract_test.cr
@@ -18,13 +18,13 @@ def write_file(output_name : String, matrix : Goishi::Matrix(UInt8))
   StumpyPNG.write(canvas, output_name)
 end
 
-def extract(input_name : String)
+def decode_qr(input_name : String)
   puts input_name
   matrix = read_file(input_name)
 
   locator = Goishi::LocatorSession.new
   locator.set_data(matrix)
-  locator.locate_qr(10) do |location|
+  locator.locate_qr(3) do |location|
     extractor = Goishi::Extractor.new(matrix)
     extracted_matrix = extractor.extract(location)
     extracted_matrix.invert if location.color == 0
@@ -37,7 +37,6 @@ def extract(input_name : String)
       extracted_matrix = extractor.extract(location)
       Goishi::QR::Decoder.decode(extracted_matrix).segments rescue next
     rescue e
-      puts e.inspect_with_backtrace
       extracted_matrix = extractor.extract(location)
       extracted_matrix.flip_tr_bl
       Goishi::QR::Decoder.decode(extracted_matrix).segments rescue next
@@ -48,14 +47,31 @@ def extract(input_name : String)
   end
 end
 
-# extract("examples/assets/test.png")
-# extract("examples/assets/skewed1.png")
-# extract("examples/assets/skewed2.png")
-# extract("examples/assets/hflipped.png")
-# extract("examples/assets/vflipped.png")
-# 0.step(to: 330, by: 30) do |i|
-#  extract("examples/assets/rotate#{i}.png")
-# end
-5.times do |i|
-  extract("examples/assets/real_world#{i + 1}.png")
+def decode_mqr(input_name : String)
+  puts input_name
+  matrix = read_file(input_name)
+
+  locator = Goishi::LocatorSession.new
+  locator.set_data(matrix)
+  locator.locate_mqr(3) do |location|
+    extractor = Goishi::Extractor.new(matrix)
+    extracted_matrix = extractor.extract(location)
+    extracted_matrix.invert if location.color == 0
+    write_file("debug.png", extracted_matrix)
+
+  end
 end
+
+# decode_qr("examples/assets/qr/test.png")
+# decode_qr("examples/assets/qr/skewed1.png")
+# decode_qr("examples/assets/qr/skewed2.png")
+# decode_qr("examples/assets/qr/hflipped.png")
+# decode_qr("examples/assets/qr/vflipped.png")
+# 0.step(to: 330, by: 30) do |i|
+#  decode_qr("examples/assets/qr/rotate#{i}.png")
+# end
+# 5.times do |i|
+#   decode_qr("examples/assets/qr/real_world#{i + 1}.png")
+# end
+
+decode_mqr("examples/assets/mqr/test.png")
