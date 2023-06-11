5f0ae7d75677d1ae4cf5dae20846d1c998edbffa
soya_daizu <soya_daizu@proton.me>
2023-06-12 02:15:14 +0900

    feat: new visualizer methods

--- src/goishi/visualizer.cr
+++ src/goishi/visualizer.cr
@@ -7,6 +7,7 @@ module Goishi
 
     @@count = 0
     @@canvas = StumpyPNG::Canvas.new(1, 1)
+    @@font : PCFParser::Font = PCFParser::Font.from_file("./ter-x14n.pcf")
 
     def set_data(data)
       @@canvas = StumpyPNG::Canvas.new(data.size_x, data.size_y) do |x, y|
@@ -15,11 +16,19 @@ module Goishi
     end
 
     def add_point(p : Point, hex : String? = nil)
+      color = hex ? StumpyPNG::RGBA.from_hex(hex) : StumpyPNG::RGBA::RED
+      # @@canvas[p.x.round_even.to_i, p.y.round_even.to_i] = color
       @@canvas.circle(p.x.to_i, p.y.to_i, 2, hex ? StumpyPNG::RGBA.from_hex(hex) : StumpyPNG::RGBA::RED, true)
     end
 
     def add_line(a : Point, b : Point, hex : String? = nil)
-      @@canvas.line(a.x, a.y, b.x, b.y, hex ? StumpyPNG::RGBA.from_hex(hex) : StumpyPNG::RGBA::RED)
+      color = hex ? StumpyPNG::RGBA.from_hex(hex) : StumpyPNG::RGBA::RED
+      @@canvas.line(a.x.round_even, a.y.round_even, b.x.round_even, b.y.round_even, color)
+    end
+
+    def add_text(p : Point, text : String, hex : String? = nil)
+      color = hex ? StumpyPNG::RGBA.from_hex(hex) : StumpyPNG::RGBA::RED
+      @@canvas.text(p.x.round_even.to_i, p.y.round_even.to_i, text, @@font, color)
     end
 
     def export
