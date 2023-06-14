e7c4de5f5b24790f61c9325074a47459febeb8ca
soya_daizu <soya_daizu@proton.me>
2023-06-14 15:42:26 +0900

    refactor: cache some Quad instance method results

--- src/goishi/data_types/quad.cr
+++ src/goishi/data_types/quad.cr
@@ -7,9 +7,18 @@ module Goishi
     getter center : Point
     getter color : UInt8
 
+    {% for x in {:a, :b, :c, :d} %}
+      {% for y in {:a, :b, :c, :d} %}
+        {% if x != y %}
+          getter {{x.id}}{{y.id}} : Point { {{y.id}} - {{x.id}} }
+        {% end %}
+      {% end %}
+    {% end %}
+
+    getter w_vec : Point { (ab + cd) / 2 }
+    getter h_vec : Point { (ac + bd) / 2 }
     getter width : Float64 { w_vec.length }
     getter height : Float64 { h_vec.length }
-    getter longest_side_length : Float64 { {ab.length, cd.length, ac.length, bd.length}.max }
 
     def initialize(pair1 : Tuple(Point, Point), pair2 : Tuple(Point, Point), @center, @color)
       if pair1[0].x <= pair2[0].x
@@ -20,16 +29,6 @@ module Goishi
         @b, @c = pair1
       end
     end
-
-    {% for x in {:a, :b, :c, :d} %}
-      {% for y in {:a, :b, :c, :d} %}
-        {% if x != y %}
-          def {{x.id}}{{y.id}}
-            {{y.id}} - {{x.id}}
-          end
-        {% end %}
-      {% end %}
-    {% end %}
 
     def intersecting_side(to_quad : Quad)
       intersection = Point.intersection(a, b, @center, to_quad.center, true)
@@ -48,17 +47,7 @@ module Goishi
       w_sin_sita = Point.angle_between(vec, w_vec).abs
       h_sin_sita = Point.angle_between(vec, h_vec).abs
 
-      return w_vec if w_sin_sita < h_sin_sita
-      return h_vec if h_sin_sita < w_sin_sita
-      w_vec
-    end
-
-    def w_vec
-      (ab + cd) / 2
-    end
-
-    def h_vec
-      (ac + bd) / 2
+      w_sin_sita <= h_sin_sita ? w_vec : h_vec
     end
   end
 end
